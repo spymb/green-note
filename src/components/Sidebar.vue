f2fbbf1932d917b47ba3ff20c6fa58a74c3aaac0
spymb <spym1933@foxmail.com>
2022-07-17 21:39:24 +0800

    引入 less

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -18,42 +18,49 @@
 import Avatar from './Avatar.vue';
 
 export default {
-  components: {Avatar}
-};
+  components: {Avatar},
 
+  methods: {
+    onLogout() {
+
+    }
+  }
+};
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 
 #sidebar {
   position: relative;
   width: 56px;
   text-align: center;
   background-color: #2c333c;
+
+  .icons {
+    margin-top: 15px;
+
+    a {
+      padding: 6px 0;
+      display: block;
+    }
+
+    .router-link-active {
+      background-color: #5e6266;
+    }
+
+  }
+
+  .logout {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .iconfont {
+    color: #fff;
+  }
 }
 
-.icons {
-  margin-top: 15px;
-}
-
-.icons a {
-  padding: 6px 0;
-  display: block;
-}
-
-.icons .router-link-active {
-  background-color: #5e6266;
-}
-
-.logout {
-  position: absolute;
-  bottom: 20px;
-  width: 100%;
-  text-align: center;
-  cursor: pointer;
-}
-
-.iconfont {
-  color: #fff;
-}
 </style>
