92f62dd0ce2dd879a6e62f0f01a22d1e9ac2c601
spymb <spym1933@foxmail.com>
2022-07-21 11:10:13 +0800

    封装 trash 接口

---
+++ src/apis/trash.js
@@ -0,0 +1,34 @@
+import request from '../helpers/request'
+import {beautifyDate} from '../helpers/date';
+
+const URL = {
+  DELETE: '/notes/:noteId/confirm',
+  REVERT: '/notes/:noteId/revert',
+  GET: '/notes/trash',
+}
+
+export default {
+  delete({ noteId }) {
+    return request(URL.DELETE.replace(':noteId', noteId), 'DELETE')
+  },
+
+  revert({ noteId }) {
+    return request(URL.REVERT.replace(':noteId', noteId), 'PATCH')
+  },
+
+  getAll() {
+    return new Promise((resolve, reject) => {
+      request(URL.GET)
+        .then(res => {
+          res.data = res.data.sort((note1, note2) => note1.createdAt < note2.createdAt ? -1 : 1)
+          res.data.forEach(note => {
+            note.newCreatedAt = beautifyDate(note.createdAt)
+            note.newUpdatedAt = beautifyDate(note.updatedAt)
+          })
+          resolve(res)
+        }).catch(err => {
+        reject(err)
+      })
+    })
+  },
+}
